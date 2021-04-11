bbc46fc6f2c5deea68775b743bddc2c195b332c9
Shenfq <shenfq95@foxmail.com>
2021-04-11 17:31:25 +0800

    feat: 添加百度统计

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,15 +3,15 @@ module.exports = {
   description: ' ',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    // ['script', {}, `
-    //   var _hmt = _hmt || [];
-    //   (function() {
-    //     var hm = document.createElement("script");
-    //     hm.src = "https://hm.baidu.com/hm.js?b887a437433180b7cdce3335aedd741d";
-    //     var s = document.getElementsByTagName("script")[0]; 
-    //     s.parentNode.insertBefore(hm, s);
-    //   })();
-    // `]
+    ['script', {}, `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?ce26c35c5c5403057ebddcb99965920a";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+    `]
   ],
   themeConfig: {
     search: true,
